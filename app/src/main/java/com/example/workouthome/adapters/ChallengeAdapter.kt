3c04ae175dc79cb446c04a3751c203d300b46325
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-05-10 09:46:05 +0300

    My first commit

---
+++ app/src/main/java/com/example/workouthome/adapters/ChallengeAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.workouthome.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.workouthome.R
+import com.example.workouthome.modules.ChallengeItem
+import kotlinx.android.synthetic.main.content_recycler_view_challenge.view.*
+
+class ChallengeAdapter(var activity: FragmentActivity?, var arrayList: ArrayList<ChallengeItem>) : RecyclerView.Adapter<ChallengeAdapter.ChallengeViewHolder>() {
+    class ChallengeViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        var descriptionChallenge: TextView = view.description_challenge
+        var iconsChallenge: ImageView = view.image_challenge
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChallengeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.content_recycler_view_challenge, parent, false)
+        return ChallengeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ChallengeViewHolder, position: Int) {
+        val challengeItem: ChallengeItem = arrayList[position]
+
+        viewHolder.descriptionChallenge.text = challengeItem.description_challenge
+        viewHolder.iconsChallenge.setImageResource(challengeItem.icons_challenge)
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+}
